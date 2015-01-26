cb67a80d6d586219b365863c0bcf1077bcc0bd6f
bashupoud <bashupoud@gmail.com>
2015-01-26 18:05:01 -0500

    Initial CheckIN of offer Project

---
+++ offersProject-110/src/com/caveofprogramming/spring/web/service/UsersService.java
@@ -0,0 +1,35 @@
+package com.caveofprogramming.spring.web.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caveofprogramming.spring.web.dao.User;
+import com.caveofprogramming.spring.web.dao.UsersDao;
+
+@Service("usersService")
+public class UsersService {
+
+	private UsersDao usersDao;
+
+	@Autowired
+	public void setUsersDao(UsersDao usersDao) {
+		this.usersDao = usersDao;
+	}
+
+	public void create(User user) {
+		usersDao.create(user);
+	}
+
+	public boolean exists(String username) {
+		// TODO Auto-generated method stub
+		return usersDao.exists(username);
+	}
+
+	public List<User> getAllUser() {
+		// TODO Auto-generated method stub		
+		return usersDao.getAllUser();
+	}
+
+}
