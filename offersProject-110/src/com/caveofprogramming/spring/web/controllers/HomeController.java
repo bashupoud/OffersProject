62e41bc989471cfd6581f55fe212d8cb34921dd0
bashupoud <bashupoud@gmail.com>
2015-01-26 18:21:23 -0500

    Adding a request mapping to the home Controller

--- offersProject-110/src/com/caveofprogramming/spring/web/controllers/HomeController.java
+++ offersProject-110/src/com/caveofprogramming/spring/web/controllers/HomeController.java
@@ -5,11 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
+	/**
+	 * Will land it to the Homepage
+	 * @return
+	 */
 	@RequestMapping("/")
 	public String showHome() {
 		return "home"; 
 	}
 	
+	
+	@RequestMapping("/registration")
+	public String showRegistration() {
+		return "registration"; 
+	}
 
 }
